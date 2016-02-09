f68d60e2fa58a743b3f6030e54ca570e8e01d6c7
Victoria Lacroix <victoria.a.lacroix@gmail.com>
2016-02-09 09:55:27 -0500

    Adding the plugin
    Signed-off-by: Victoria Lacroix <victoria.a.lacroix@gmail.com>

---
+++ orion.umple.js
@@ -0,0 +1,57 @@
+
+// Headers for the plugin provider
+var provider = new orion.PluginProvider({
+  name: "Orion.Umple",
+  version: "alpha",
+  description: ""
+});
+
+// Adds a new content type that recognizes Umple textual models
+provider.registerService("orion.core.contenttype", {}, {
+  contentTypes: [
+    {
+      id: "text/umple",
+      name: "Textual Umple Model",
+      extension: [
+        "ump"
+      ],
+      // extends must be a string as it is otherwise reserved in Javascript
+      "extends": "text/plain"
+    }
+  ]
+});
+
+// Adds a grammar for highlighting code and keywords in Umple models
+provider.registerService("orion.edit.highlighter", {}, {
+  id: "orion.umple",
+  contentTypes: ["text/umple"],
+  patterns: [
+    // Matcing braces and parenthesis from Orion's library
+    { include: "orion.lib#string_doubleQuote" },
+    { include: "orion.lib#string_singleQuote" },
+    { include: "orion.lib#brace_open" },
+    { include: "orion.lib#brace_close" },
+    { include: "orion.lib#bracket_open" },
+    { include: "orion.lib#bracket_close" },
+    { include: "orion.lib#parenthesis_open" },
+    { include: "orion.lib#parenthesis_close" },
+
+    // Numbers and c-style comments, which Umple uses.
+    { include: "orion.lib#number_decimal" },
+    { include: "orion.c-like#comments" },
+
+    // Keywords
+    {
+      name: "keyword.other.reserved",
+      match: "\\b(?:class|interface|namespace|external|generate|strictness|association|trait|depend|use|isA|lazy|immutable|const|settable|internal|autounique|defaulted|before|after|Integer|Float|String|Double|Boolean|Date|Time|->|--|entry|do|exit|singleton|trace|tracer)\\b"
+    },
+
+    // For extra code, straight import for now like in UmpleOnline.
+    { include: "orion.java" },
+    { include: "orion.cpp" },
+    { include: "orion.php" }
+  ]
+});
+
+// Get connected to Orion so that services can actually be provided.
+provider.connect();
