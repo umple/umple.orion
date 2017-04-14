3d0849d48c05f71d83b759c6a9bb9a0c4d900707
Noah Murad <noahmurad@hotmail.com>
2017-04-14 03:20:59 -0400

    Add class to generate arguments from request

---
+++ umple-orion-server/src/main/java/org/cruise/umple/orion/server/ArgGen.java
@@ -0,0 +1,32 @@
+package org.cruise.umple.orion.server;
+
+public class ArgGen {
+	private Request request;
+	
+	private static final String UMPLE_GENERATE_FLAG = "-g";
+	private static final String UMPLE_PATH_FLAG = "--path";
+	private static final String UMPLE_GENERATED_FOLDER_POSTFIX = "-Gen-Umple";
+	
+	public ArgGen(Request r){
+		request = r;
+	}
+	
+	public String[] generateUmpleArgs(){
+		String[] umpleArgs;
+		String language = request.getLanguage();
+		String filename = request.getAbsoluteFilename();
+		
+		if(!language.equals("")){
+			umpleArgs = new String[]{
+					UMPLE_GENERATE_FLAG, language, 
+					UMPLE_PATH_FLAG, filename.substring(filename.lastIndexOf('/')+1, filename.lastIndexOf('.'))
+							+ "-" + language + UMPLE_GENERATED_FOLDER_POSTFIX,
+					filename}; 
+		}else{
+			umpleArgs = new String[]{filename};
+		}
+		
+		return umpleArgs;
+	}
+
+}
